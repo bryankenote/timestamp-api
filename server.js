e9211e542fc80cfa975b78389b36cb8cea3a51fc
Bryan <brykenote@gmail.com>
2017-03-12 02:23:57 +0000

    add missing semicolons

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 let express = require('express');
-let app = express()
+let app = express();
 let port = 8080;
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('index');
-})
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port+'!')
-})
+  console.log('Example app listening on port '+port+'!');
+});
