f77ac58496cf898675efc717fa7824837ca3f665
Bryan <brykenote@gmail.com>
2017-03-12 03:31:46 +0000

    return date on get /:date

--- server.js
+++ server.js
@@ -8,6 +8,31 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+let months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+
+app.get('/:date', function (req, res) {
+  let param = req.params.date;
+  let time;
+  if (isNaN(param))
+    time = param;
+  else
+    time = Number(param) * 1000;
+  
+  let d = new Date(time);
+  let output = {
+    unix: d.getTime() / 1000,
+  };
+  if (months[d.getMonth()])
+    output.natural = `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+  else
+    output.natural = null;
+    
+  res.end(JSON.stringify(output));
+});
+
 app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
 });
